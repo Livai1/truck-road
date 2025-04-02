58dc66d63354b8081f5fe29f3594edc7f768a7ad
Erick Levi <ericklevi564@gmail.com>
2025-04-02 13:19:10 -0300

    Acrescenta um offset a camera

--- TruckRoad/Assets/Scripts/FollowPlayer.cs
+++ TruckRoad/Assets/Scripts/FollowPlayer.cs
@@ -4,9 +4,10 @@ public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private GameObject player;
 
-    
+    private Vector3 offSet = new Vector3(0,4.4f,-7.4f);
+
     void Update()
     {
-        transform.position = player.transform.position +  new Vector3(0,4.4f,-7.4f);
+        transform.position = player.transform.position + offSet;
     }
 }
